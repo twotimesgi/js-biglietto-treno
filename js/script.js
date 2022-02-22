fcf14d578525b52462431671cc9a41cbf7d59812
twotimesgi <luigibardellagerbi@mail.com>
2022-02-22 10:51:27 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ let totalPrice = pricePerKm * km;
 if (!isNaN(km) && !isNaN(age)){
   if(age < 18){
     totalPrice = totalPrice*0.8;
-  }else if(age > 65){
+  }else if(age >= 65){
     totalPrice = totalPrice*0.6;
   }
     document.getElementById("output").innerHTML = "Il prezzo totale è di " + totalPrice.toFixed(2) + "€.";
